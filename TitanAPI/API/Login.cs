b033eac11ea1f7d5c9ab1aefb72262d6bdd42fa3
marc-cr1810 <marc.cookerussell@gmail.com>
2022-08-18 01:02:18 +1000

    Test for logging in

--- TitanAPI/API/Login.cs
+++ TitanAPI/API/Login.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TitanAPI.Encryption;
 using TitanAPI.Web;
 
 namespace TitanAPI.API
@@ -19,10 +21,14 @@ namespace TitanAPI.API
 
     public class Login
     {
-        private static AuthenticationWebService authentication = new AuthenticationWebService();
+        private static AuthenticationWebServiceWrapper authentication = new AuthenticationWebServiceWrapper();
 
         public static string Logon(LoginCredentials credentials)
         {
+            string userid = credentials.UserName;
+            string password = credentials.Password;
+            string dealerid = credentials.DealerID;
+
             Version version = Utils.Utilities.GetHighestDotNetVersionFromRegistry() ?? new Version("0.0");
             ComputerInfo computerInfo = new ComputerInfo();
             string servicePack = Environment.OSVersion.ServicePack;
@@ -33,11 +39,21 @@ namespace TitanAPI.API
             int num3 = ((Screen.AllScreens != null) ? Screen.AllScreens.Length : (-1));
             string machine = string.Concat(Environment.MachineName, ";", version, ";", text, ";", Environment.ProcessorCount, ";", computerInfo.TotalPhysicalMemory / 1024uL / 1024uL, ";", Assembly.GetEntryAssembly().GetName().Name, ";", num, ";", num2, ";", num3);
 
+            TimeSpan utcOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
             authentication.Url = WebService.GetWebServerEndpoint("Authentication");
+            authentication.Timeout = 1000000;
+            authentication.KeepAlive = true;
 
+            string logonResponse = authentication.LogonAuthentication(CustomEncryption.Encrypt(userid),
+                CustomEncryption.Encrypt(password),
+                CustomEncryption.Encrypt(machine),
+                CustomEncryption.Encrypt(dealerid),
+                string.Empty,
+                utcOffset.Ticks.ToString(),
+                CustomEncryption.Encrypt(WindowsIdentity.GetCurrent().Name),
+                -1);
 
-
-            return "";
+            return logonResponse;
         }
     }
 }
